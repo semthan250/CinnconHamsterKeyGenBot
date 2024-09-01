bfaff89232400d60f4fd57a20e3567ade12f43f9
semthan250 <153456001+semthan250@users.noreply.github.com>
2024-09-01 20:45:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,38 +5,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const games = {
         1: {
-            name: 'Riding Extreme 3D',
-            appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-            promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-            eventsDelay: 30000,
-            attemptsNumber: 25,
+            name: 'ZooPolis',
+            appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            eventsDelay: 21000,
+            attemptsNumber: 20,
         },
         2: {
             name: 'Chain Cube 2048',
             appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
             promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
-            eventsDelay: 30000,
+            eventsDelay: 21000,
             attemptsNumber: 20
         },
         4: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-            eventsDelay: 30000,
-            attemptsNumber: 15,
+            eventsDelay: 21000,
+            attemptsNumber: 20,
         },
         5: {
             name: 'Merge Away',
             appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
             promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
-            eventsDelay: 30000,
-            attemptsNumber: 25,
+            eventsDelay: 21000,
+            attemptsNumber: 20,
         },
         6: {
             name: 'Twerk Race 3D',
             appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-            eventsDelay: 30000,
+            eventsDelay: 21000,
             attemptsNumber: 20,
             
         },
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Polysphere',
             appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
-            eventsDelay: 20000,
+            eventsDelay: 21000,
             attemptsNumber: 16,
             
 	},
@@ -52,16 +52,16 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Mow and Trim',
             appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
             promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-            eventsDelay: 20000,
+            eventsDelay: 21000,
             attemptsNumber: 15,
             
 	},
         9: {
-            name: 'Mud Racing',
-            appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            eventsDelay: 20000,
-            attemptsNumber: 15,
+            name: 'Gangs Wars',
+            appToken: 'b6de60a0-e030-48bb-a551-548372493523',
+            promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
+            eventsDelay: 40000,
+            attemptsNumber: 20,
             
 	},
         3: {
@@ -70,10 +70,8 @@ document.addEventListener('DOMContentLoaded', () => {
             promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
             eventsDelay: 23000,
             attemptsNumber: 16,
-            
-	}
+        },
     };
-
 
     const startBtn = document.getElementById('startBtn');
     const keyCountSelect = document.getElementById('keyCountSelect');
@@ -335,15 +333,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     document.getElementById('creatorChannelBtn').addEventListener('click', () => {
-        window.open('https://telegram.me/Cancincon', '_blank');
+        window.open('https://telegram.me/aLcapone7s', '_blank');
     });
 
     telegramAdminBtn.addEventListener('click', () => {
-        window.open('https://telegram.me/HamsterKeyDuyuru', '_blank');
+        window.open('https://telegram.me/aLcapone7s', '_blank');
     });
 	
 	telegramChannelBtn.addEventListener('click', () => {
-        window.open('https://telegram.me/HamsterKeyDuyuru', '_blank');
+        window.open('https://telegram.me/aLcapone7s', '_blank');
     });
 
     document.getElementById('ShowKeysBtn').addEventListener('click', () => {
